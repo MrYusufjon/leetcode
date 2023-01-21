4f53f9ac581ab0c0c37bf9faca3a6c2c3a58aa50
Yusufjon <52450380+MrYusufjon@users.noreply.github.com>
2023-01-21 22:34:43 +0000

    Sync LeetCode submission - Plus One (typescript)

---
+++ problems/plus_one/solution.ts
@@ -0,0 +1,17 @@
+function plusOne(digits: number[]): number[] {
+    let notUsed = true;
+    for(let i=digits.length-1;i>=0;i--) {
+        if(!notUsed) break;
+        if(digits[i]+1==10) {
+            digits[i] = 0;
+        }
+        else {
+            digits[i] = digits[i]+1
+            notUsed = false;
+        }
+    }
+    if(notUsed) {
+        digits = [1,...digits]
+    }
+    return digits;
+};
