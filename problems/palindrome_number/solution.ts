5180422e445a293473020a7af49ed9b89fa7ff8a
Yusufjon <52450380+MrYusufjon@users.noreply.github.com>
2023-01-21 21:25:49 +0000

    Sync LeetCode submission - Palindrome Number (typescript)

---
+++ problems/palindrome_number/solution.ts
@@ -0,0 +1,10 @@
+function isPalindrome(x: number): boolean {
+    let itself = x;
+    let reversed = 0;
+    while (x>0) {
+        const digit = x%10;
+        reversed = reversed*10+digit;
+        x = Math.floor(x/10);
+    }
+    return itself == reversed
+};
